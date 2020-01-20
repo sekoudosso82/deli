72ab7003e2df3fb747d1f23bfa8629f44f4cfd5e
sekoudosso82 <sekou.dosso82@gmail.com>
2020-01-20 00:54:16 -0500

    total coding commit

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 
-var sum = document.getElementById("label1").innerHTML 
-        document.getElementById("addLB").addEventListener("click", function(){
-            
-            sum = parseInt(sum, 10); 
-            sum+=1;
-            document.getElementById("label1").innerHTML = sum;
-            
+// let totalp = document.querySelector("#total").innerHTML;
+var totalp = 20;
+
+// sandwich
+let centerA = document.querySelector("#addCB");
+let centerS = document.querySelector("#subCB");
+let centerQ = document.querySelector("#centerInput") ;
+
+
+        centerA.addEventListener("click", () => {
+            centerQ.value = parseInt(centerQ.value)+1;
+         
         });
-        // console.log(document.getElementById("label1").innerHTML);
-//         console.log(sum);
+
+        
+        centerS.addEventListener("click", () => {
+            if (parseInt(centerQ.value)>0) {
+                centerQ.value = parseInt(centerQ.value)-1;
+            }
+        });
+totalp = totalp + parseInt(centerQ)*5;
+
+document.querySelector("h6").innerHTML= "Total : $" +  totalp ;
+
+// phily-cheese
+let rightA = document.querySelector("#addRB");
+let rightS = document.querySelector("#subRB");
+let rightQ = document.querySelector("#rightInput") ;
+
+        rightA.addEventListener("click", () => {
+            rightQ.value = parseInt(rightQ.value)+1;
+
+        });
+
+        rightS.addEventListener("click", () => {
+            if (parseInt(rightQ.value)>0) {
+                rightQ.value = parseInt(rightQ.value)-1;
+            }
+        });
+
+
+
+        // bugger
+let addButton = document.querySelector("#addLB");
+let removeButton = document.querySelector("#subLB");
+let quantity = document.querySelector("#leftInput") ;
+
+        addButton.addEventListener("click", () => {
+            quantity.value = parseInt(quantity.value)+1;
+
+        });
+
+        removeButton.addEventListener("click", () => {
+            if (parseInt(quantity.value)>0) {
+                quantity.value = parseInt(quantity.value)-1;
+            }
+        });
+            
+
+
+
 
     
 
