98efd55339ca0102bde9907f8ad71f8bc850f68b
sekoudosso82 <sekou.dosso82@gmail.com>
2020-01-20 09:27:04 -0500

    place order coding commit

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 
-// let totalp = document.querySelector("#total").innerHTML;
-var totalp = 20;
+let totalp = 0;
 
+// bugger
+let leftA = document.querySelector("#addLB");
+let leftS = document.querySelector("#subLB");
+let leftQ = document.querySelector("#leftInput") ;
+        
+        leftA.addEventListener("click", () => {
+            leftQ.value = parseInt(leftQ.value)+1;
+            totalp += 5;
+            document.querySelector("h6").innerHTML= "Total : $" +  totalp ;
+        });
+        
+        leftS.addEventListener("click", () => {
+            if (parseInt(leftQ.value)>0) {
+                leftQ.value = parseInt(leftQ.value)-1;
+                totalp -=   5;
+                document.querySelector("h6").innerHTML= "Total : $" +  totalp ;
+            }
+        });
 // sandwich
 let centerA = document.querySelector("#addCB");
 let centerS = document.querySelector("#subCB");
 let centerQ = document.querySelector("#centerInput") ;
 
-
         centerA.addEventListener("click", () => {
             centerQ.value = parseInt(centerQ.value)+1;
-         
+            totalp += 4;
+            document.querySelector("h6").innerHTML= "Total : $" +  totalp ;
         });
-
-        
         centerS.addEventListener("click", () => {
             if (parseInt(centerQ.value)>0) {
                 centerQ.value = parseInt(centerQ.value)-1;
+                totalp -= 4;
+                document.querySelector("h6").innerHTML= "Total : $" +  totalp ;
             }
         });
-totalp = totalp + parseInt(centerQ)*5;
-
-document.querySelector("h6").innerHTML= "Total : $" +  totalp ;
 
 // phily-cheese
 let rightA = document.querySelector("#addRB");
@@ -30,38 +44,28 @@ let rightQ = document.querySelector("#rightInput") ;
 
         rightA.addEventListener("click", () => {
             rightQ.value = parseInt(rightQ.value)+1;
-
-        });
+            totalp += 3;
+            document.querySelector("h6").innerHTML= "Total : $" +  totalp ;
+       });
 
         rightS.addEventListener("click", () => {
             if (parseInt(rightQ.value)>0) {
                 rightQ.value = parseInt(rightQ.value)-1;
-            }
+                totalp -=   3;
+                document.querySelector("h6").innerHTML= "Total : $" +  totalp ;
+           }
         });
 
+let orderButton = document.querySelector("#placeOrder");
+        orderButton.addEventListener("click", () =>{
+            leftQ.value = 0;
+            centerQ.value = 0;
+            rightQ.value = 0;
+            totalp = 0;
+            document.querySelector("h6").innerHTML= "Total : $" +  totalp ;
 
-
-        // bugger
-let addButton = document.querySelector("#addLB");
-let removeButton = document.querySelector("#subLB");
-let quantity = document.querySelector("#leftInput") ;
-
-        addButton.addEventListener("click", () => {
-            quantity.value = parseInt(quantity.value)+1;
-
-        });
-
-        removeButton.addEventListener("click", () => {
-            if (parseInt(quantity.value)>0) {
-                quantity.value = parseInt(quantity.value)-1;
-            }
-        });
             
-
-
-
-
-    
+        });
 
 
     
